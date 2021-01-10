f801a9ab47413ca511a45ae2835efb992cfa00d9
Blugil <dagnello@asu.edu>
2021-01-10 14:35:26 -0700

    expose cors to all addresses

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ app.use(cors())
 const io = require("socket.io")(http, {
     cors: {
         //origin of request
-        origin: ["https://che55.online/", "https://nostalgic-keller-1abde6.netlify.app", "http://localhost:3000", "*"],
+        origin: "*",
         methods: ["GET", "POST"],
         credentials: true,
     }
