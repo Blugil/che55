54de87405eecfdfa1f616d67c0b189eac5ed9c5b
shufflesninja <isaact23@vt.edu>
2021-01-09 20:25:26 -0500

    Add move validation

--- frontend/src/components/home.tsx
+++ frontend/src/components/home.tsx
@@ -1,20 +1,48 @@
 import React, { useState } from "react";
 import Chessboard from "chessboardjsx";
-import { ChessInstance } from "chess.js";
+import { ChessInstance, ShortMove } from "chess.js";
 const Chess = require("chess.js");
 
 const Home : React.FC = () => {
 
+    // Create a chess.js object to generate legal moves
     const [chess] = useState<ChessInstance>(
         new Chess("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
     );
 
+    const [fen, setFen] = useState(chess.fen());
+
+    // Handle a player move
+    const handleMove = (move: ShortMove) => {
+        if (chess.move(move)) {
+            setTimeout(() => {
+                // Get random move from computer
+                const moves = chess.moves();
+                if (moves.length > 0) {
+                    const computerMove = moves[Math.floor(Math.random() * moves.length)];
+                    chess.move(computerMove);
+                    setFen(chess.fen());
+                }
+            }, 300);
+
+            setFen(chess.fen());
+        }
+    }
+
+    // Return HTML to be injected by DOM
     return (
         <div>
             <h1>home.tsx</h1>
             <Chessboard
                 width={400}
-                position="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+                position={fen}
+                onDrop={(move) =>
+                    handleMove({
+                        from: move.sourceSquare,
+                        to: move.targetSquare,
+                        promotion: "q",
+                    })
+                }
             />
         </div>
     );
